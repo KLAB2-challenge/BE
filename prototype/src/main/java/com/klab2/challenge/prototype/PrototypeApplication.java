415c223e025d092060bf443e6444eefa505b49a6
GwonPyo <ngp9441@gmail.com>
2023-10-31 00:06:22 +0900

    add springBoot Files

---
+++ prototype/src/main/java/com/klab2/challenge/prototype/PrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.klab2.challenge.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeApplication.class, args);
+	}
+
+}
---
+++ prototype/src/test/java/com/klab2/challenge/prototype/PrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klab2.challenge.prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
