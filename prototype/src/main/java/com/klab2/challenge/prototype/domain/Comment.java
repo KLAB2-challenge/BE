72761219e21c8603e35fef2b62a8c5d1d7cad240
LEESEOKJIN <33368126+LEE5J@users.noreply.github.com>
2023-11-01 01:57:18 +0900

    Update Comment.java

--- prototype/src/main/java/com/klab2/challenge/prototype/domain/Comment.java
+++ prototype/src/main/java/com/klab2/challenge/prototype/domain/Comment.java
@@ -1,4 +1,34 @@
 package com.klab2.challenge.prototype.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
+    @Id
+    @GeneratedValue
+    @Column(name = "comment_id")
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long user_id;
+
+    @Column(name = "pp_id")
+    private Long pp_id;
+
+    @Column(name = "comment")
+    private String comment;
+
+    public void setComment(String comment){
+        this.comment = comment;
+    }
+
+
 }
