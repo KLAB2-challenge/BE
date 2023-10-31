ba4fafe654b403f2cfaf61bb4d29b7478ec89e28
LEESEOKJIN <33368126+LEE5J@users.noreply.github.com>
2023-11-01 01:56:46 +0900

    Update CommentController.java

--- prototype/src/main/java/com/klab2/challenge/prototype/controller/CommentController.java
+++ prototype/src/main/java/com/klab2/challenge/prototype/controller/CommentController.java
@@ -1,4 +1,25 @@
 package com.klab2.challenge.prototype.controller;
 
+
+import com.klab2.challenge.prototype.dto.response.GetAllCommentsResponse;
+import com.klab2.challenge.prototype.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @GetMapping("/getAllComments")
+    @ResponseBody
+    public ResponseEntity<GetAllCommentsResponse> getAllComments(){
+        //GetAllCommentsResponse response = commentService;
+    }
 }
