5c4ec6ca680db93534675ec73a7fe63f7f94465d
GwonPyo <ngp9441@gmail.com>
2023-11-26 03:14:50 +0900

    add codes

--- prototype/src/test/java/com/klab2/challenge/prototype/service/ChallengeServiceTest.java
+++ prototype/src/test/java/com/klab2/challenge/prototype/service/ChallengeServiceTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.klab2.challenge.prototype.domain.Challenge;
